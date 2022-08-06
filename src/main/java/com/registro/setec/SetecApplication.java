a83e8876680a6e7bcd86053965f5b379f568dbe4
patocastill0 <patolzincastillo@gmail.com>
2022-08-05 20:35:59 -0500

    Comentario

---
+++ src/main/java/com/registro/setec/SetecApplication.java
@@ -0,0 +1,13 @@
+package com.registro.setec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registro/setec/SetecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registro.setec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
